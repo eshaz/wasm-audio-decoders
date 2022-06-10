1ee51dce8a6e4f7ca8944d2f7249ea39b64ed732
Ethan Halsall <ethanhalsall11@augustana.edu>
2022-06-09 21:45:46 -0500

    refactor: simplify web worker command selection

--- demo/ogg-opus-decoder.js
+++ demo/ogg-opus-decoder.js
@@ -265,76 +265,57 @@
           `(${((_options, _Decoder, _WASMAudioDecoderCommon, _EmscriptenWASM) => {
           // We're in a Web Worker
 
-          let moduleResolve;
-          const modulePromise = new Promise((resolve) => {
-            moduleResolve = resolve;
-          });
-
-          let decoder;
+          // setup Promise that will be resolved once the WebAssembly Module is received
+          let decoder,
+            moduleResolve,
+            modulePromise = new Promise((resolve) => {
+              moduleResolve = resolve;
+            });
 
           self.onmessage = ({ data: { id, command, data } }) => {
-            switch (command) {
-              case "module":
-                Object.defineProperties(_Decoder, {
-                  WASMAudioDecoderCommon: { value: _WASMAudioDecoderCommon },
-                  EmscriptenWASM: { value: _EmscriptenWASM },
-                  module: { value: data },
-                  isWebWorker: { value: true },
-                });
+            let messagePromise = modulePromise,
+              messagePayload = { id },
+              transferList;
 
-                decoder = new _Decoder(_options);
-                moduleResolve();
-              case "ready":
-                modulePromise.then(() =>
-                  decoder.ready.then(() => {
-                    self.postMessage({
-                      id,
-                    });
-                  })
-                );
-                break;
-              case "free":
-                decoder.free();
-                self.postMessage({
-                  id,
-                });
-                break;
-              case "reset":
-                decoder.reset().then(() => {
-                  self.postMessage({
-                    id,
-                  });
-                });
-                break;
-              case "decode":
-              case "decodeFrame":
-              case "decodeFrames":
-                const { channelData, samplesDecoded, sampleRate } = decoder[
-                  command
-                ](
+            if (command === "module") {
+              Object.defineProperties(_Decoder, {
+                WASMAudioDecoderCommon: { value: _WASMAudioDecoderCommon },
+                EmscriptenWASM: { value: _EmscriptenWASM },
+                module: { value: data },
+                isWebWorker: { value: true },
+              });
+
+              decoder = new _Decoder(_options);
+              moduleResolve();
+            } else if (command === "free") {
+              decoder.free();
+            } else if (command === "ready") {
+              messagePromise = messagePromise.then(() => decoder.ready);
+            } else if (command === "reset") {
+              messagePromise = messagePromise.then(() => decoder.reset());
+            } else {
+              // "decode":
+              // "decodeFrame":
+              // "decodeFrames":
+              Object.assign(
+                messagePayload,
+                decoder[command](
                   // detach buffers
                   Array.isArray(data)
                     ? data.map((data) => new Uint8Array(data))
                     : new Uint8Array(data)
-                );
-
-                self.postMessage(
-                  {
-                    id,
-                    channelData,
-                    samplesDecoded,
-                    sampleRate,
-                  },
-                  // The "transferList" parameter transfers ownership of channel data to main thread,
-                  // which avoids copying memory.
-                  channelData.map((channel) => channel.buffer)
-                );
-                break;
-              default:
-                this.console.error(
-                  "Unknown command sent to worker: " + command
-                );
+                )
+              );
+              // The "transferList" parameter transfers ownership of channel data to main thread,
+              // which avoids copying memory.
+              transferList = messagePayload.channelData.map(
+                (channel) => channel.buffer
+              );
             }
+
+            messagePromise.then(() =>
+              self.postMessage(messagePayload, transferList)
+            );
           };
         }).toString()})(${JSON.stringify(
           options
