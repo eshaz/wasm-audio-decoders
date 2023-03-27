a8e0a4591e7513e87beade2601766f5835b23949
Ethan Halsall <ethanhalsall11@augustana.edu>
2023-03-27 18:21:46 -0500

    fix: detect nodejs module runtime

--- src/common/src/WASMAudioDecoderWorker.js
+++ src/common/src/WASMAudioDecoderWorker.js
@@ -10,77 +10,77 @@ export default class WASMAudioDecoderWorker extends getWorker() {
     let source = WASMAudioDecoderCommon.modules.get(Decoder);
 
     if (!source) {
-      const webworkerSourceCode =
-        "'use strict';" +
-        // dependencies need to be manually resolved when stringifying this function
-        `(${((_Decoder, _WASMAudioDecoderCommon, _EmscriptenWASM) => {
-          // We're in a Web Worker
+      let type = "text/javascript",
+        isNode,
+        webworkerSourceCode =
+          "'use strict';" +
+          // dependencies need to be manually resolved when stringifying this function
+          `(${((_Decoder, _WASMAudioDecoderCommon, _EmscriptenWASM) => {
+            // We're in a Web Worker
 
-          // setup Promise that will be resolved once the WebAssembly Module is received
-          let decoder,
-            moduleResolve,
-            modulePromise = new Promise((resolve) => {
-              moduleResolve = resolve;
-            });
-
-          self.onmessage = ({ data: { id, command, data } }) => {
-            let messagePromise = modulePromise,
-              messagePayload = { id },
-              transferList;
-
-            if (command === "init") {
-              Object.defineProperties(_Decoder, {
-                WASMAudioDecoderCommon: { value: _WASMAudioDecoderCommon },
-                EmscriptenWASM: { value: _EmscriptenWASM },
-                module: { value: data.module },
-                isWebWorker: { value: true },
+            // setup Promise that will be resolved once the WebAssembly Module is received
+            let decoder,
+              moduleResolve,
+              modulePromise = new Promise((resolve) => {
+                moduleResolve = resolve;
               });
 
-              decoder = new _Decoder(data.options);
-              moduleResolve();
-            } else if (command === "free") {
-              decoder.free();
-            } else if (command === "ready") {
-              messagePromise = messagePromise.then(() => decoder.ready);
-            } else if (command === "reset") {
-              messagePromise = messagePromise.then(() => decoder.reset());
-            } else {
-              // "decode":
-              // "decodeFrame":
-              // "decodeFrames":
-              Object.assign(
-                messagePayload,
-                decoder[command](
-                  // detach buffers
-                  Array.isArray(data)
-                    ? data.map((data) => new Uint8Array(data))
-                    : new Uint8Array(data)
-                )
+            self.onmessage = ({ data: { id, command, data } }) => {
+              let messagePromise = modulePromise,
+                messagePayload = { id },
+                transferList;
+
+              if (command === "init") {
+                Object.defineProperties(_Decoder, {
+                  WASMAudioDecoderCommon: { value: _WASMAudioDecoderCommon },
+                  EmscriptenWASM: { value: _EmscriptenWASM },
+                  module: { value: data.module },
+                  isWebWorker: { value: true },
+                });
+
+                decoder = new _Decoder(data.options);
+                moduleResolve();
+              } else if (command === "free") {
+                decoder.free();
+              } else if (command === "ready") {
+                messagePromise = messagePromise.then(() => decoder.ready);
+              } else if (command === "reset") {
+                messagePromise = messagePromise.then(() => decoder.reset());
+              } else {
+                // "decode":
+                // "decodeFrame":
+                // "decodeFrames":
+                Object.assign(
+                  messagePayload,
+                  decoder[command](
+                    // detach buffers
+                    Array.isArray(data)
+                      ? data.map((data) => new Uint8Array(data))
+                      : new Uint8Array(data)
+                  )
+                );
+                // The "transferList" parameter transfers ownership of channel data to main thread,
+                // which avoids copying memory.
+                transferList = messagePayload.channelData
+                  ? messagePayload.channelData.map((channel) => channel.buffer)
+                  : [];
+              }
+
+              messagePromise.then(() =>
+                self.postMessage(messagePayload, transferList)
               );
-              // The "transferList" parameter transfers ownership of channel data to main thread,
-              // which avoids copying memory.
-              transferList = messagePayload.channelData
-                ? messagePayload.channelData.map((channel) => channel.buffer)
-                : [];
-            }
-
-            messagePromise.then(() =>
-              self.postMessage(messagePayload, transferList)
-            );
-          };
-        }).toString()})(${Decoder}, ${WASMAudioDecoderCommon}, ${EmscriptenWASM})`;
-
-      const type = "text/javascript";
+            };
+          }).toString()})(${Decoder}, ${WASMAudioDecoderCommon}, ${EmscriptenWASM})`;
 
       try {
-        // browser
-        source = URL.createObjectURL(new Blob([webworkerSourceCode], { type }));
-      } catch {
-        // nodejs
-        source = `data:${type};base64,${Buffer.from(
-          webworkerSourceCode
-        ).toString("base64")}`;
-      }
+        isNode = typeof process.versions.node !== "undefined";
+      } catch {}
+
+      source = isNode
+        ? `data:${type};base64,${Buffer.from(webworkerSourceCode).toString(
+            "base64"
+          )}`
+        : URL.createObjectURL(new Blob([webworkerSourceCode], { type }));
 
       WASMAudioDecoderCommon.modules.set(Decoder, source);
     }
