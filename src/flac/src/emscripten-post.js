825ed8f79291910061fa171caa80fc655e03ea24
Ethan Halsall <ethanhalsall11@augustana.edu>
2023-10-09 00:43:59 -0500

    chore(flac): patch release

--- src/flac/src/emscripten-post.js
+++ src/flac/src/emscripten-post.js
@@ -1,7 +1,7 @@
 this.ready = new Promise((resolve) => {
   ready = resolve;
 }).then(() => {
-  this.HEAP = buffer;
+  this.HEAP = wasmMemory.buffer;
   this.malloc = _malloc;
   this.free = _free;
   this.create_decoder = _create_decoder;
