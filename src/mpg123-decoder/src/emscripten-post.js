d585e53dcbf9da05768fabfdab2601e298d513d5
Ethan Halsall <ethanhalsall11@augustana.edu>
2023-10-09 00:46:23 -0500

    chore(mpg123-decoder): patch release

--- src/mpg123-decoder/src/emscripten-post.js
+++ src/mpg123-decoder/src/emscripten-post.js
@@ -1,7 +1,7 @@
 this.ready = new Promise((resolve) => {
   ready = resolve;
 }).then(() => {
-  this.HEAP = buffer;
+  this.HEAP = wasmMemory.buffer;
   this.malloc = _malloc;
   this.free = _free;
   this.mpeg_frame_decoder_create = _mpeg_frame_decoder_create;
